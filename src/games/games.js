bc28de1be60f55e066e1bc9e8dfbfdbfdfd1d0b2
Saul Winer <karatesaul@me.com>
2022-12-06 17:13:49 -0800

    Replace AngularJS files with Angular base

--- src/games/games.js
+++
@@ -1,8 +0,0 @@
-angular.module('karatesaul').component('gamesPage', {
-    templateUrl: 'src/games/games.html',
-    controller: GamesPageCtrl
-});
-
-function GamesPageCtrl () {
-    this.games = [];
-}
--- src/main/main.js
+++
@@ -1,6 +0,0 @@
-angular.module('karatesaul').component('mainPage', {
-    templateUrl: 'src/main/main.html',
-    controller: MainPageCtrl
-});
-
-function MainPageCtrl () {}
